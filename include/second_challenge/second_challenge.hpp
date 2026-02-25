09a2fcac431541fd53623dcc1173671b7d7864f6
Rei Shibata <shibacho24@gmail.com>
2026-02-25 09:16:18 +0900

    first commit

---
+++ include/second_challenge/second_challenge.hpp
@@ -0,0 +1,41 @@
+#ifndef FIRST_CHALLENGE_HPP
+#define FIRST_CHALLENGE_HPP
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <functional>  // bind & placeholders用
+#include <memory>      // SharedPtr用
+#include <optional>    // has_value()用
+
+class SecondChallenge : public rclcpp::Node
+{
+    public:
+        SecondChallenge();
+        // void process();
+
+        // コールバック関数
+        void timer_callback();
+        void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+
+        // 関数
+        bool can_move();                        // センサ情報（今回はodom）を取得できているかの確認用
+        bool is_goal();                         // 終了判定
+        double calc_distance();                 // 進んだ距離を計算
+        void run(float velocity, float omega);  // roombaの制御入力を決定
+        void set_cmd_vel();                     // 並進速度と旋回速度を計算
+
+        // 変数
+        int hz_ = 10;             // 制御周期
+        double goal_dist_ = 1.0;  // ゴールまでの距離
+        double velocity_ = 0.3;   // 直進時の並進速度
+        std::optional<sensor_msgs::msg::LaserScan> scan_;  // optional型で定義することによりscanをsubできたかの判定も同時に行う
+        geometry_msgs::msg::Twist cmd_vel_;
+
+        // Pub & Sub
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;   // scan
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;  // 制御入力
+};
+
+#endif  // SECOND_CHALLENGE_HPP
---
+++ src/second_challenge.cpp
@@ -0,0 +1,81 @@
+#include  "second_challenge/second_challenge.hpp"
+
+SecondChallenge::SecondChallenge()
+: Node("second_challenge")
+{
+    // --- パブリッシャーの作成 ---
+    // トピック名: "cmd_vel"
+    cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", hz_);
+  
+    // --- サブスクライバーの作成 ---
+    // トピック名: "scan"
+    // 実行する関数: scan_callback (この後、クラス内に定義する必要がある)
+    scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "scan", hz_, std::bind(&SecondChallenge::scan_callback, this, std::placeholders::_1));
+
+    // firstで使用したグローバル変数の定義にtimerを追加
+}
+
+void SecondChallenge::timer_callback()
+{
+    // 繰り返したい関数を定義
+    // 距離を判定して、速度をセットしパブリッシュする
+    set_cmd_vel();
+}
+
+void SecondChallenge::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+    // ここにレーザースキャンのコールバックの処理を書く
+    scan_ = *msg;  // 受け取ったメッセージをクラス内の変数に保存
+}
+
+bool SecondChallenge::can_move()
+{
+    return scan_.has_value();
+}
+
+bool SecondChallenge::is_goal()
+{
+    return calc_distance() >= goal_dist_;
+}
+
+double SecondChallenge::calc_distance()
+{
+    // 壁からの距離や壁に垂直な向きのセンサが何番目か調べる
+    // ロボットの真正面にある一番近い障害物までの距離: ranges配列の中央要素
+    float range_ahead = scan_.value().ranges[scan_.value().ranges.size() / 2];
+    return range_ahead;
+  
+    /* 方向
+    bearing = msg.angle_min + i * msg.angle_max / len(msg.ranges);*/
+
+    /* スキャナで検出した一番近い障害物までの距離: ranges配列の最小値
+    closest_range = min(msg.ranges) */
+
+}
+
+void SecondChallenge::run(float velocity, float omega)
+{
+    // ここにロボットを動かす処理を書く
+    // 速度データを格納
+    cmd_vel_.linear.x = velocity;
+    cmd_vel_.angular.z = omega;
+
+    // パブリッシュを実行
+    cmd_vel_pub_->publish(cmd_vel_);
+}
+
+void SecondChallenge::set_cmd_vel()
+{
+    // ここにcmd_velをセットする処理を書く
+    if(is_goal())
+    {
+    // 停止
+        run(0.0, 0.0);
+    }
+    else
+    {
+    // 直進 (0.1m/s, 0.0rad/s)
+        run(velocity_, 0.0);
+    }
+}
